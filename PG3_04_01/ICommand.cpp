465d433f79402f1e76de8cae21bfa86c39135b53
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2024-01-24 16:41:40 +0900

    プロジェクト ファイルを追加します。

---
+++ PG3_04_01/ICommand.cpp
@@ -0,0 +1,21 @@
+﻿#include "ICommand.h"
+#include "Player.h"
+void RightCommand::Exec(Player& player)
+{
+	player.RightMove();
+}
+
+void LeftCommand::Exec(Player& player)
+{
+	player.LeftMove();
+}
+
+void UpCommand::Exec(Player& player)
+{
+	player.UpMove();
+}
+
+void DownCommand::Exec(Player& player)
+{
+	player.DownMove();
+}
